90e7b51849c8945168e200534037b21e23444e92
gitjunh <41998632+gitjunh@users.noreply.github.com>
2020-01-29 20:24:35 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 // For command line git tutorial
-// Github modification
+// First: Github modification
+// Second: Local repository modification
 
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
